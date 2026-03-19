d0427ed12e2c8e369c6a2125c848fafcb96dca2d
vikhyat-1999 <122731569+vikhyat-1999@users.noreply.github.com>
2026-03-20 00:51:51 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,8 @@ exports.login = async (req, res) => {
     const { email, password } = req.body;
     console.log(req.body);
     const user = await User.findOne({ email });
-    console.log(user);
+    console.log("DB NAME:", User.db.name);
+    console.log("User found:", user);
     if (!user) {
       return res.status(400).json({ message: "User not found" });
     }

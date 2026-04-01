df4126253cb44ca2408383df4f77e26bda4c1a77
vikhyat-1999 <122731569+vikhyat-1999@users.noreply.github.com>
2026-04-01 08:46:33 +0530

    Add files via upload

---
+++ controllers/locationController.js
@@ -0,0 +1,58 @@
+// controllers/locationController.js
+
+const Location = require("../models/Location");
+
+// ➤ Add location
+exports.createLocation = async (req, res) => {
+  try {
+
+    const { locationName, zipCode } = req.body;
+
+    const location = await Location.create({
+      locationName,
+      zipCode
+    });
+
+    res.status(201).json({
+      message: "Location added successfully",
+      location
+    });
+
+  } catch (err) {
+
+    if (err.code === 11000) {
+      return res.status(400).json({
+        message: "Location already exists"
+      });
+    }
+
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+// ➤ Get all locations (for dropdown)
+exports.getLocations = async (req, res) => {
+  try {
+
+    const locations = await Location.find({ isActive: true });
+
+    res.json({ locations });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+exports.deleteLocation = async (req, res) => {
+  try {
+
+    const { id } = req.params;
+
+    await Location.findByIdAndDelete(id);
+
+    res.json({ message: "Location deleted" });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};

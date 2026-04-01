59a0c95cded8ecd97635961cffedd49efbdce56b
vikhyat-1999 <122731569+vikhyat-1999@users.noreply.github.com>
2026-04-01 08:45:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use("/api/jobs", require("./routes/jobRoutes"));
 app.use("/api/reports", require("./routes/reportRoutes"));
 app.use("/api/notifications", require("./routes/notificationRoutes"));
 app.use("/api/banks", require("./routes/bankRoutes"));
+app.use("/api/locations", require("./routes/locationRoutes"));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI)

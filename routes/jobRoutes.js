172411cb14c1945d26f5517fbb0df2c529664fb4
vikhyat-1999 <122731569+vikhyat-1999@users.noreply.github.com>
2026-04-04 14:54:47 +0530

    Update jobRoutes.js

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createJob,getMyJobs,getAdminJobs  } = require("../controllers/jobController");
+const { createJob,getMyJobs,getAdminJobs,getJobById  } = require("../controllers/jobController");
 const { verifyToken, authorizeRoles } = require("../middleware/authMiddleware");
 
 router.post(

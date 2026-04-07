1bf2b2750443a113e02838012ed74c495a8c3f7d
vikhyat-1999 <122731569+vikhyat-1999@users.noreply.github.com>
2026-04-07 13:24:08 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const router = express.Router();
-const { createUser,getEngineersByCity,getAdminsByCity,getRegisteredUsers,toggleUserStatus } = require("../controllers/userController");
+const { createUser,getEngineersByCity,getAdminsByCity,getRegisteredUsers,toggleUserStatus,deleteUser } = require("../controllers/userController");
 const { verifyToken, authorizeRoles } = require("../middleware/authMiddleware");
 const { getMyJobs } = require("../controllers/jobController");
 

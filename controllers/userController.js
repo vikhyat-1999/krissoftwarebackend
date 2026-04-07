175661e53cc3fd4944d91e477b07dc68901fc1e9
vikhyat-1999 <122731569+vikhyat-1999@users.noreply.github.com>
2026-04-07 13:20:46 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -112,3 +112,18 @@ exports.toggleUserStatus = async (req, res) => {
 
   res.json({ message: "Updated" });
 };
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ message: "User deleted successfully" });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};

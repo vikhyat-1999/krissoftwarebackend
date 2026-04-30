8ada2a7f283e8a34c6a28ec9db8d79028f0c4ab5
vikhyat-1999 <122731569+vikhyat-1999@users.noreply.github.com>
2026-04-30 09:19:11 +0530

    Update notificationController.js

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,8 +1,51 @@
 const Notification = require("../models/Notfication");
 exports.getMyNotifications = async (req, res) => {
-  const notifications = await Notification.find({
-    user: req.user.id
-  }).sort({ createdAt: -1 });
+  try {
 
-  res.json({ notifications });
+    const notifications = await Notification.find({
+      user: req.user.id
+    })
+      .sort({ createdAt: -1 })
+      .limit(20);
+
+    const unreadCount = await Notification.countDocuments({
+      user: req.user.id,
+      isRead: false
+    });
+
+    res.json({
+      notifications,
+      unreadCount
+    });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+exports.markAllNotificationsRead = async (req, res) => {
+  try {
+
+    await Notification.updateMany(
+      { user: req.user.id, isRead: false },
+      { isRead: true }
+    );
+
+    res.json({ message: "All marked as read" });
+
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+exports.markOneRead = async (req, res) => {
+  try {
+    await Notification.findByIdAndUpdate(
+      req.params.id,
+      { isRead: true }
+    );
+
+    res.json({ message: "Marked as read" });
+
+  } catch (err) {
+    res.status(500).json({ message: "Error" });
+  }
 };
